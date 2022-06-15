75b135b36ebb870f30ccbbceb7d1d1c13b944ad9
Patey0911 <44579714+Patey0911@users.noreply.github.com>
2022-06-15 17:55:42 +0300

    Add Canada

--- Form9.cs
+++ Form9.cs
@@ -35,7 +35,7 @@ namespace F1
 
             objChart.AxisY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
             objChart.AxisY.Minimum = 0;
-            objChart.AxisY.Maximum = 250;
+            objChart.AxisY.Maximum = 300;
 
             chart2.Series.Clear();
             string nume_echipa;

1969fb05d0e8f400392b6b1d4d9e0dc55dc7ca36
Patey0911 <44579714+Patey0911@users.noreply.github.com>
2022-08-15 14:57:25 +0300

    Update max plots

--- Form8.cs
+++ Form8.cs
@@ -36,7 +36,7 @@ namespace F1
 
             objChart.AxisY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
             objChart.AxisY.Minimum = 0;
-            objChart.AxisY.Maximum = 250;
+            objChart.AxisY.Maximum = 300;
 
             chart1.Series.Clear();
 

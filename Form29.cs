c039ff59915772b91e7a8d16bf17b5dee3a1939e
Patey0911 <44579714+Patey0911@users.noreply.github.com>
2023-04-26 12:30:06 +0300

    Add Baku

--- Form29.cs
+++ Form29.cs
@@ -35,7 +35,7 @@ namespace F1
 
             objChart.AxisY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
             objChart.AxisY.Minimum = 0;
-            objChart.AxisY.Maximum = 100;
+            objChart.AxisY.Maximum = 200;
 
             chart2.Series.Clear();
             string nume_echipa;
